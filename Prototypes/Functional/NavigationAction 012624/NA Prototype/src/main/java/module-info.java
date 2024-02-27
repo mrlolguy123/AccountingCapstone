d646cea298180fefdae900bed9a4882cbe2add43
as9219 <amansahu9000@gmail.com>
2024-02-27 10:30:16 -0500

    Moved Directory

--- Prototypes/Functional/NavigationAction 012624/NA Prototype/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.napr {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.napr to javafx.fxml;
-    exports com.example.napr;
-}
