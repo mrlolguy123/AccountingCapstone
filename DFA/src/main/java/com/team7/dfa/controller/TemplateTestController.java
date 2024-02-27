439387c32844bf95295f53e15d65c0db3b9c0fbd
Danny Le <24dannle@gmail.com>
2024-02-26 20:32:19 -0500

    Workable Logout Button
    All it does it is close the program

--- DFA/src/main/java/com/team7/dfa/controller/TemplateTestController.java
+++ DFA/src/main/java/com/team7/dfa/controller/TemplateTestController.java
@@ -1,7 +1,17 @@
 package com.team7.dfa.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class TemplateTestController {
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    protected void logoutClicked(ActionEvent event)
+    {
+        System.exit(0);
+    }
 }
