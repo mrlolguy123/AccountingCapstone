9cc766364c43ede20d3d134b7833d298f59d2f2b
Aman Sahu <amansahu9000@gmail.com>
2024-03-20 12:00:51 -0400

    updated dynamic path

--- DFA/src/main/java/com/team7/dfa/controller/HomePageController.java
+++ DFA/src/main/java/com/team7/dfa/controller/HomePageController.java
@@ -1,15 +1,10 @@
 package com.team7.dfa.controller;
 
-import com.team7.dfa.TemplateTestApplication;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,27 +32,33 @@ public class HomePageController extends ParentController {
     public String currentDir = Paths.get("").toAbsolutePath().toString();
     public String graphWorkingDirectory = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "generated_graphs";
 
-    @FXML
-    private void generateGraphs() {
-        generateGraphsButton.setOnAction(event -> {
-            String pythonScriptPath = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "scripts" + File.separator + "generate_graphs.py";
 
-            try {
-                ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
-                //processBuilder.directory(new File(pythonWorkingDirectory)); //this is unused??
-                Process process = processBuilder.start();
-                process.waitFor();
-                updateGraphImages();
-                System.out.println("Successfully Executed");
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+    private void runScript(){
+        try{
+            String pythonScriptPath = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "scripts" + File.separator + "generate_graphs.py";
+            ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
+            //processBuilder.directory(new File(pythonWorkingDirectory)); //this is unused??
+            Process process = processBuilder.start();
+            process.waitFor();
+
+        } catch (IOException | InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
     private void updateGraphImages() {
         //graph1.setImage(new Image("file:///C:/Users/Aman%20Sahu/Desktop/Capstone%202024/AccountingCapstone/DFA/src/main/python/generated_graphs/sample_BoxPlot.png"));
-        graph2.setImage(new Image("file:///C:/Users/Aman%20Sahu/Desktop/Capstone%202024/AccountingCapstone/DFA/src/main/python/generated_graphs/sample_BarGraph.png"));
-        graph3.setImage(new Image("file:///C:/Users/Aman%20Sahu/Desktop/Capstone%202024/AccountingCapstone/DFA/src/main/python/generated_graphs/sample_ScatterPlot.png"));
+        graph2.setImage(new Image("file:///"+ graphWorkingDirectory + File.separator + "testingbar.png"));
+        graph3.setImage(new Image("file:///"+ graphWorkingDirectory + File.separator + "testingscatter.png"));
+    }
+
+    @FXML
+    private void generateGraphs() {
+        generateGraphsButton.setOnAction(event -> {
+            System.out.println(graphWorkingDirectory);
+            runScript();
+            updateGraphImages();
+            System.out.println("Successfully Executed");
+        });
     }
 }
