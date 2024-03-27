5b1a6f15132d4cdc714f3ceefd78cbe43ca8df39
Aman Sahu <amansahu9000@gmail.com>
2024-03-26 21:34:48 -0400

    added graph object
    added a callabel graph object with its specific parameters

--- DFA/src/main/java/com/team7/dfa/controller/HomePageController.java
+++ DFA/src/main/java/com/team7/dfa/controller/HomePageController.java
@@ -1,17 +1,13 @@
 package com.team7.dfa.controller;
 
-import javafx.event.ActionEvent;
+import com.team7.dfa.model.Graph;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomePageController extends ParentController {
 
@@ -24,11 +20,6 @@ public class HomePageController extends ParentController {
     @FXML
     private Button generateGraphsButton;
 
-    public String currentDir = Paths.get("").toAbsolutePath().toString();
-    public String graphWorkingDirectory = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "generated_graphs";
-
-    public String pythonScriptPath = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "scripts" + File.separator + "generate_graphs.py";
-
     @FXML
     private void generateGraphButton() {
         generateGraphsButton.setOnAction(event -> {
@@ -37,35 +28,10 @@ public class HomePageController extends ParentController {
             String x_name = "year";
             String y_name = "totalprofit";
             String graphName = "home_profit_graph";
-            generateGraph(sqlCommand, graphChoice, x_name, y_name, graphName);
-            updateGraphImage(graphName);
+            Graph sample_graph = new Graph();
+            sample_graph.generateGraph(sqlCommand, graphChoice, x_name, y_name, graphName);
+            sample_graph.updateGraphImage(graph1, graphName);
             System.out.println("Successfully Executed");
         });
-    }
-
-    @FXML
-    private void updateGraphImage(String graphName) {
-        graph1.setImage(new Image("file:///"+ graphWorkingDirectory + File.separator + graphName + ".png"));
-    }
-
-
-    public void generateGraph(String sqlCommand, String graphChoice, String x_name, String y_name, String graphName){
-        try{
-            List<String> commands = new ArrayList<>();
-            commands.add("python");
-            commands.add(pythonScriptPath);
-            commands.add(sqlCommand);
-            commands.add(graphChoice);
-            commands.add(x_name);
-            commands.add(y_name);
-            commands.add(graphName);
-
-            ProcessBuilder processBuilder = new ProcessBuilder(commands);
-            Process process = processBuilder.start();
-            process.waitFor();
-
-        } catch (IOException | InterruptedException e){
-            e.printStackTrace();
-        }
     }
 }
