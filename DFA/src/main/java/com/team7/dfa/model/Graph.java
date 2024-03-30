c403788deb9eea9ccd2a9afe5d6cf51ca6c573f9
Aman Sahu <amansahu9000@gmail.com>
2024-03-30 01:48:54 -0400

    updated documentation
    updated the documentation of the graph model and modified the constructor. improved homepage UI

--- DFA/src/main/java/com/team7/dfa/model/Graph.java
+++ DFA/src/main/java/com/team7/dfa/model/Graph.java
@@ -9,18 +9,34 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * The Graph class represents a graph generator for the DFA application.
+ * It generates graphs based on specified parameters and updates the graph image.
+ * The types of graphs you can generate are:
+ * "1" - Scatter Plot
+ * "2" -
+ */
 public class Graph {
-    private String sqlCommand;
-    private String graphChoice;
-    private String x_name;
-    private String y_name;
-    private String col_name;
-    private String graphName;
+    private final String sqlCommand; // SQL Command for data retrieval
+    private final String graphChoice; // Type of graph to generate
+    private final String x_name; // X-axis label in sqlCommand table
+    private final String y_name; // Y-axis label in sqlCommand table
+    private final String graphName; // Name of the graph generated
+
+    // Paths for Python scripts and generated graphs
     public String currentDir = Paths.get("").toAbsolutePath().toString();
     public String pythonScriptPath = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "scripts" + File.separator + "generate_graphs.py";
     public String graphWorkingDirectory = currentDir + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "generated_graphs";
 
+
+    /**
+     * Constructs a Graph object with specified parameters and generates the graph.
+     * @param sqlCommand SQL command for data retrieval
+     * @param graphChoice Type of graph to generate
+     * @param x_name X-axis label in sqlCommand table
+     * @param y_name Y-axis label in sqlCommand table
+     * @param graphName Name of the generated graph
+     */
     public Graph(String sqlCommand, String graphChoice, String x_name, String y_name, String graphName) {
         this.sqlCommand = sqlCommand;
         this.graphChoice = graphChoice;
@@ -30,15 +46,24 @@ public class Graph {
         generateGraph();
     }
 
-    public Graph(String sqlCommand, String graphChoice, String col_name, String graphName){
+    /**
+     * Constructs a Graph object with specified parameters and generates the pie graph.
+     * @param sqlCommand SQL command for data retrieval
+     * @param col_name Column name for pie-graph generation
+     * @param graphName Name of the generated graph
+     */
+    public Graph(String sqlCommand, String col_name, String graphName){
         this.sqlCommand = sqlCommand;
-        this.graphChoice = graphChoice;
+        this.graphChoice = "4";
         this.x_name = col_name;
         this.y_name = "";
         this.graphName = graphName;
         generateGraph();
     }
 
+    /**
+     * Generates the graph based on the specified parameters using a Python script.
+     */
     private void generateGraph(){
         try{
             List<String> commands = new ArrayList<>();
@@ -58,6 +83,10 @@ public class Graph {
         }
     }
 
+    /**
+     * Updates the graph image displayed in the application.
+     * @param graph_image ImageView component to display the graph image
+     */
     public void updateGraphImage(ImageView graph_image) {
         graph_image.setImage(new Image("file:///"+ graphWorkingDirectory + File.separator + this.graphName + ".png"));
     }
