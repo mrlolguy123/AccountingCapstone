83246ed51f91fb261e81f1d1e37426f85799b2e6
Aman Sahu <amansahu9000@gmail.com>
2024-04-15 00:36:18 -0400

    added line graph

--- DFA/src/main/python/scripts/generate_graphs.py
+++ DFA/src/main/python/scripts/generate_graphs.py
@@ -8,12 +8,15 @@ import logging
 
 sql_statement = sys.argv[1]
 graph_choice = int(sys.argv[2])
-graph_x_name = sys.argv[3]
-graph_y_name = sys.argv[4]
-graph_name = sys.argv[5]
+graph_name = sys.argv[3]
+graph_x_name = sys.argv[4]
+graph_y_name = sys.argv[5]
+graph_y2_name = sys.argv[6]
 
 # # Configuration for Logging File. Returns any errors with execution
-logging.basicConfig(filename='graphErrorLog.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s\n')
+logging.basicConfig(filename='graphErrorLog.log', level=logging.ERROR,
+                    format='%(asctime)s - %(levelname)s - %(message)s\n')
+
 
 def getDataFrame(query):
     username = "capstoneAdmin"
@@ -78,6 +81,7 @@ def make_bar(df, name, height, width, x_name, y_name):
     except Exception as e:
         raise RuntimeError("An error occurred while making bar graph: %s" % e)
 
+
 def make_scatter(df, name, height, width, x_name, y_name):
     """
     Generates a scatter plot based on the provided DataFrame and saves it as an image file.
@@ -106,6 +110,7 @@ def make_scatter(df, name, height, width, x_name, y_name):
         plt.savefig(graphPathDirectory() + name + ".png", dpi=100)
     except Exception as e:
         raise RuntimeError("An error occurred while making scatter plot: %s" % e)
+
 
 def make_line(df, name, height, width, x_name, y_name):
     """
@@ -136,6 +141,40 @@ def make_line(df, name, height, width, x_name, y_name):
     except Exception as e:
         raise RuntimeError("An error occurred while making line graph: %s" % e)
 
+
+def make_double_line(df, name, height, width, x_name, y_name, y2_name):
+    """
+    Generates a double line graph based on the provided DataFrame and saves it as an image file.
+
+    Args:
+        :param df: The DataFrame containing the data to be plotted.
+        :param name: The name of the graph and the output image file.
+        :param height: The height of the generated figure.
+        :param width: The width of the generated figure.
+        :param x_name: The label for the x-axis.
+        :param y1_name: The label for the y-axis of the first line.
+        :param y2_name: The label for the y-axis of the second line.
+
+    Returns:
+        None
+
+    Note:
+        This function requires the seaborn and matplotlib libraries to be installed.
+    """
+    try:
+        plt.figure(figsize=(width, height))
+        sns.set_style("white")
+        sns.lineplot(x=x_name, y=y_name, data=df, label=y_name)
+        sns.lineplot(x=x_name, y=y2_name, data=df, label=y2_name)
+        plt.xlabel(x_name)
+        plt.ylabel("Values")
+        plt.title(name)
+        plt.legend()
+        plt.savefig(graphPathDirectory() + name + ".png", dpi=100)
+    except Exception as e:
+        raise RuntimeError("An error occurred while making double line graph: %s" % e)
+
+
 def make_pie_chart(df, name, height, width, x_name):
     """
     Generates a scatter plot based on the provided DataFrame and saves it as an image file.
@@ -164,14 +203,15 @@ def make_pie_chart(df, name, height, width, x_name):
         patches, texts, autotexts = ax.pie(data, labels=data.index, colors=colors, autopct='%1.1f%%', startangle=90,
                                            pctdistance=0.85, shadow=True, wedgeprops={'edgecolor': 'black'})
 
-        #adjust position of percentage labels
+        # adjust position of percentage labels
         for autotext in autotexts:
-            autotext.set_size(11)  #font size for precentages
+            autotext.set_size(11)  # font size for precentages
 
         plt.title(name)
         plt.savefig(graphPathDirectory() + name + ".png", dpi=80)
     except Exception as e:
         raise RuntimeError("An error occurred while making pie chart: %s" % e)
+
 
 if __name__ == '__main__':
     try:
@@ -184,7 +224,10 @@ if __name__ == '__main__':
         elif graph_choice == 3:
             make_line(getDataFrame(str(sql_statement)), str(graph_name), 7, 14, graph_x_name, graph_y_name)
 
-        elif graph_choice == 4 and graph_y_name == "":
+        elif graph_choice == 3.5:
+            make_double_line(getDataFrame(str(sql_statement)), str(graph_name), 7, 14, graph_x_name, graph_y_name, graph_y2_name)
+
+        elif graph_choice == 4 and graph_y_name == "*":
             make_pie_chart(getDataFrame(str(sql_statement)), str(graph_name), 6, 12, graph_x_name)
     except Exception as e:
         logging.error("An error occurred: %s", e, exc_info=True)
